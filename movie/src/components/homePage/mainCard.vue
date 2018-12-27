d53b5099abb549bf60d50aee4d9234442040745c
Shicript <shicript@ShicriptdeMacBook-Pro.local>
2018-12-27 11:09:20 +0800

    .

--- movie/src/components/homePage/mainCard.vue
+++ movie/src/components/homePage/mainCard.vue
@@ -1,26 +1,23 @@
 <template>
-    <el-row>
-        <el-col :span="14" :offset="5">
-            <div id = 'head-pic'>
-           <img width="102%" height="400px" style="border-radius:30px;" src="@/assets/head.jpg">
-            <span style="position: absolute; top: 90%; left: 5%;color:white;">aaaaaaaa</span>
-            </div>
-        </el-col>
-    </el-row>
-    
+  <el-row>
+    <el-col :span="16" :offset="4">
+      <div id="head-pic">
+        <img width="102%" height="350px" style="border-radius:30px;" src="@/assets/head.jpg">
+        <span style="position: absolute; top: 90%; left: 5%;color:white;">aaaaaaaa</span>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 <script>
 export default {
-    name: 'mainCard',
-    data(){
-        return {
-
-        }
-    }   
-}
+  name: "mainCard",
+  data() {
+    return {};
+  }
+};
 </script>
 <style>
-#head-pic{
-    position: relative;
+#head-pic {
+  position: relative;
 }
 </style>
